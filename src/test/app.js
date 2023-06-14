52f2756d9c85e2fc221fdfc7cc1593086b9d2a30
Noah Jutz <noahjutz@tutanota.de>
2023-06-14 18:50:54 +0200

    Set up MongoDB Node Driver for tests

--- src/test/app.js
+++ src/test/app.js
@@ -1,9 +1,20 @@
+import "dotenv/config";
 import { assert } from "chai";
 import axios from "axios";
+import { MongoClient } from "mongodb";
 
 const client = axios.create({ baseURL: "http://localhost:8080" });
+const mongoClient = new MongoClient(process.env.MONGO_URL);
 
 suite("app", () => {
+  suiteSetup(async () => {
+    await mongoClient.connect();
+  });
+
+  suiteTeardown(async () => {
+    await mongoClient.close();
+  });
+
   test("GET / returns Hello, World!", async () => {
     const res = await client.get("/");
     assert.equal(res.data, "Hello, World!");
