7d8b8d631a17653a21a8d63f5ae4df3edcf18e6b
Noah Jutz <noahjutz@tutanota.de>
2023-06-14 16:45:06 +0200

    Install hapi

--- src/main/app.js
+++ src/main/app.js
@@ -1,1 +1,10 @@
-// todo
+import Hapi from "@hapi/hapi";
+
+const server = Hapi.server({ host: "localhost", port: 8080 });
+
+const init = async () => {
+  await server.start();
+  console.log("Server running at " + server.info.uri);
+};
+
+init();
--- src/test/app.js
+++ src/test/app.js
@@ -1,7 +1,7 @@
 import { assert } from "chai";
 import axios from "axios";
 
-const client = axios.create();
+const client = axios.create({ baseURL: "http://localhost:8080" });
 
 suite("app", () => {
   test("hello world", async () => {
