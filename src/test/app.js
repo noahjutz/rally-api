1d98055a8bb63167feb6b1cb4b884b2522571c10
Noah Jutz <noahjutz@tutanota.de>
2023-06-14 16:30:48 +0200

    Install axios

--- src/test/app.js
+++ src/test/app.js
@@ -1,7 +1,10 @@
 import { assert } from "chai";
+import axios from "axios";
+
+const client = axios.create();
 
 suite("app", () => {
-  test("hello world", () => {
-    throw "not yet implemented";
+  test("hello world", async () => {
+    const res = await client.get("/");
   });
 });
