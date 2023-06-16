15fe335fed33f03feb840ffa7af34d51f7a45414
Noah Jutz <noahjutz@tutanota.de>
2023-06-16 14:15:15 +0200

    Simplify test/app.js

--- src/test/app.js
+++ src/test/app.js
@@ -16,24 +16,12 @@ suite("app", () => {
     await mongoClient.close();
   });
 
-  teardown(async () => {
-    await mongoClient.db("test").dropDatabase();
-  });
-
   test("GET / returns hello collection", async () => {
-    await hellos.insertOne({ text: "Hello, World!" });
-
     const actual = (await client.get("/")).data;
     const expected = (await hellos.find().toArray()).map((e) => {
       e._id = e._id.toString();
       return e;
     }); // todo reformat
-
-    console.log("From MongoDB Node Driver:");
-    console.log(expected);
-
-    console.log("From mongoose:");
-    console.log(actual);
 
     assert.deepEqual(actual, expected);
   });
